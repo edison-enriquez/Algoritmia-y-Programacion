7446f8213ebdaed1b37a9db5dcddbff763fca3df
edison-enriquez <62826864+edison-enriquez@users.noreply.github.com>
2023-09-28 01:12:20 +0000

    Main de prueba

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int numero;
+    cout << "Escriba el numero: " <<endl;
+    cin >> numero;
+    cout << "El numero ingresado es:" << numero << endl;
+
+    return 0;
+}
